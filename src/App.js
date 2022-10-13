005d5bfecf5e76726685e7021b78c0c03b4def4a
ale <jano231982@gmail.com>
2022-10-12 23:18:44 -0300

    login & back ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-=======
-import { useAuth0 } from "@auth0/auth0-react";
-
-import { LoginButton } from "./Login";
-import { LogoutButton } from "./Logout";
-import { Profile } from "./Profile";
-import logo from "./logo.svg";
-import imageUrl from './img/backR&M.jpg';
->>>>>>> 6517f0e05ef7277649ebd72a6f3ab77af35724e1
 import "./App.css";
 import "./components/Inicio"
 import Inicio from "./components/Inicio";
@@ -17,19 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
         <Inicio/>
-=======
-        <img src={imageUrl} className="imagenBack" alt="Background" />
-        {isAuthenticated ? (
-          <>
-            <Profile />
-            <LogoutButton />
-          </>
-        ) : (
-          <LoginButton />
-        )}
->>>>>>> 6517f0e05ef7277649ebd72a6f3ab77af35724e1
       </header>
     </div>
   );
